3728ed9ec597938c7b05b47da834d21e83d43d44
Thamires Mendes <mlthami.mendes@hotmail.com>
2016-06-21 15:03:34 -0300

    Aula do dia 21/06

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            List<Carro> carros = new List<Carro>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                Carro c = new Carro();
+                c.Placa = Console.ReadLine();
+                c.Ano = int.Parse(Console.ReadLine());
+                c.Modelo = Console.ReadLine();
+                carros.Add(c);
+            }
+              
+        }
+    }
+}
