23cffde39fb02a6f22507f82614d37d333e4058d
ThamiresIn <mlthami.mendes@gmail.com>
2016-06-28 13:40:41 -0300

    Aula 28/06

---
+++ Pessoa/Pessoa/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pessoa
+{
+    class Pessoa
+    {
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public string cpf { get; set; }
+        public string telefone { get; set; }
+        public DateTime dataNascimento { get; set; }
+
+        public Pessoa() { }
+        public Pessoa( string nome , string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+        }
+        public Pessoa (string cpf)
+        {
+            this.cpf = cpf;
+        }
+
+    }
+}
---
+++ Pessoa/Pessoa/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Pessoa();
+            new Pessoa("Thamires", "Mendes");
+            new Pessoa("0358243770");
+        }
+    }
+}
